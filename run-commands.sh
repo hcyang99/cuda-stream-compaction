9a9d2e7a47ef77f97ab4ede8708b5e63108180bd
Haichao Yang <hcyang@ucsd.edu>
2022-02-11 12:13:00 -0800

    init

---
+++ run-commands.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Change directory (DO NOT CHANGE!)
+repoDir=$(dirname "$(realpath "$0")")
+echo $repoDir
+cd $repoDir
+
+# Recompile if necessary (DO NOT CHANGE!)
+mkdir -p build
+cd build
+cmake ..
+make -j4
+
+## Basic run to map the first 40 reads in the reads.fa in batches of 10 reads
+## HINT: may need to change values for the assignment tasks. You can create a sequence of commands
+./scan
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/opt/launch-sh/bin/launch.sh -c 8 -g 1 -m 64 -i yatisht/ece284-wi22:latest -f ${HOME}/prefix-scan/run-commands.sh
